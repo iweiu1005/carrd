b870e7021a9122f160f44dab811b4264ef67f98d
woody <153756651+iweiu1005@users.noreply.github.com>
2025-11-07 12:38:38 +0330

    Update receipt.js

--- receipt.js
+++ receipt.js
@@ -1,12 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const today = new Date();
-    const formattedDate = today.getFullYear() + "." + 
-                         String(today.getMonth() + 1).padStart(2, "0") + "." + 
-                         String(today.getDate()).padStart(2, "0");
-    const dateElement = document.querySelector(".Datep");
-    if (dateElement) {
-        dateElement.innerHTML = `Date: ${formattedDate}`;
-        // یا
-        // dateElement.textContent = `Date: ${formattedDate}`;
-    }
-});
+                const today = new Date();
+                const formattedDate = today.getFullYear() + "." + String(today.getMonth() + 1).padStart(2, "0") + "." + String(today.getDate()).padStart(2, "0");
+
+                const dateElement = document.querySelector(".Datep");
+                if (dateElement) {
+                    dateElement.innerHTML = `Date: ${formattedDate}`;
+                }
+            });
